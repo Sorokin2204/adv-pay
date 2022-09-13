e46de2a40812a7eba727729c21366fe4638e9fc7
Lolipop098gt <identityv.ru@gmail.com>
2022-09-13 07:55:24 +0300

    остальные страницы

--- src/pages/SupportPage.jsx
+++ src/pages/SupportPage.jsx
@@ -13,13 +13,19 @@ const SupportPage = () => {
         <Container>
           <Box sx={{ marginTop: 4 }}>
             <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'start' }}>
-              <img src="/logo.webp" style={{ height: '200px', width: '100%', maxWidth: ' 350px', objectFit: 'cover' }} />
-
               <Typography variant="h4" sx={{ mt: 2 }}>
-                Поддержка
+                Техническая поддержка
               </Typography>
-              <Typography variant="body1" sx={{ maxWidth: '600px', mt: 2 }}>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis natus perspiciatis porro laudantium! Expedita, officia accusantium omnis iusto nobis eveniet quidem dignissimos. Voluptatibus reiciendis quae, fugit repellat nulla enim expedita!
+              <Typography variant="body1" sx={{ maxWidth: '100%', mt: 2 }}>
+               По любым вопросам сервиса вы можете обращаться:<br/>
+ВК - <a href="https://vk.com/im?sel=-213480825" target="_blank">Написать нам</a> <br/>
+email - donategold.s@gmail.com<br/><br/>
+Режим работы: ежедневно с 10:00 до 20:00<br/><br/><br/>
+
+
+<img src="/v_white_on_transp_ru.png"/><br/>
+<a href="https://passport.webmoney.ru/asp/certview.asp?wmid=386255644072" target="_blank"><span>Проверить аттестат</span></a>
+
               </Typography>
             </Box>
           </Box>
