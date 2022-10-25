88debcf93edcf34babf38ab601c1dd7c319fa77f
Lolipop098gt <identityv.ru@gmail.com>
2022-10-25 20:15:51 +0300

    vidos2

--- src/pages/DonatePage.jsx
+++ src/pages/DonatePage.jsx
@@ -15,18 +15,10 @@ const DonatePage = () => {
             <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'start' }}>
               <Typography variant="h4" sx={{ mt: 2 }}>
                 Инструкция
-              </Typography>
-			  <Typography variant="h5" sx={{ mt: 2 }}>
-                Пополняем баланс
-              </Typography>
-              <Typography variant="body1" sx={{ maxWidth: '100%', mt: 2 }}>
-                После регистрации у вас будет доступен личный кабинет пользователя. На данный момент донатить можно только с баланса личного кабинета.
-				<br />
-				Для пополнения баланса нажмите на кнопку карты с плюсиком.
-              </Typography>
-			  <Typography variant="body1" sx={{ maxWidth: '100%', mt: 2 }}>
-			  <iframe src="https://vk.com/video_ext.php?oid=-213480825&id=456239024&hash=05f99572efb915b0&hd=4" width="1920" height="1080" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
-			   </Typography>
+              </Typography
+<Typography variant="body1" sx={{ maxWidth: '100%', mt: 2 }}>
+        <iframe style={{width: '100%'}} src="https://vk.com/video_ext.php?oid=-213480825&id=456239024&hash=05f99572efb915b0&hd=4" width="1920" height="1080" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+         </Typography>
             </Box>
           </Box>
         </Container>
