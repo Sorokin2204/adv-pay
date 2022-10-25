09004174662d808d44b024d45965e35f010b1753
Lolipop098gt <identityv.ru@gmail.com>
2022-10-25 21:22:14 +0300

    vidos6

--- src/pages/DonatePage.jsx
+++ src/pages/DonatePage.jsx
@@ -15,8 +15,8 @@ const DonatePage = () => {
             <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'start' }}>
               <Typography variant="h4" sx={{ mt: 2 }}>
                 Инструкция
-              </Typography
-<Typography variant="body1" sx={{ maxWidth: '100%', width: '100%', mt: 2 }}>
+              </Typography>
+<Typography variant="body1" sx={{ maxWidth: '100%', width: '100%', height: '200px', mt: 2 }}>
         <iframe style={{width: '100%',height: '200px'}} src="https://vk.com/video_ext.php?oid=-213480825&id=456239024&hash=05f99572efb915b0&hd=4" width="1920" height="1080" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
          </Typography>
             </Box>
